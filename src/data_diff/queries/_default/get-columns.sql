7c94bd2944bbefd42ec0be4980732545be7bd00b
Bill Wallis <bill@tasman.ai>
2026-04-10 20:20:49 +0100

    feat: add license

--- src/data_diff/queries/_default/get-columns.sql
+++ src/data_diff/queries/_default/get-columns.sql
@@ -3,4 +3,6 @@ select
     ordinal_position,
     data_type
 from {{ database }}.INFORMATION_SCHEMA.COLUMNS
-where table_name = '{{ table }}'
+where 1=1
+    and table_schema = '{{ schema }}'
+    and table_name = '{{ table }}'
